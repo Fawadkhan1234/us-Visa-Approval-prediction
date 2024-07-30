6d884dabcbc460835d2b2a2be648bf54bad2f723
Fawad Khan <Fawadkhan1234>
2024-07-30 18:59:39 +0500

    changes in template.py file

--- template.py
+++ template.py
@@ -33,24 +33,13 @@ list_of_files = [
     "config/schema.yaml" 
 ]
 
-# for filepath in list_of_files:
-#     filepath = Path(filepath)
-#     filedir, filename = os.path.split(filepath)
-#     if filepath != "":
-#         os.makedirs(filedir, exist_ok= True)
-#         if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-#             with open(filepath, 'w') as f:
-#                 pass
-#         else:
-#             print(f"file is already present at: {filepath}")
-
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir = filepath.parent
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-    if not filepath.exists() or filepath.stat().st_size == 0:
-        with open(filepath, 'w') as f:
-            pass
-    else:
-        print(f"file is already present at: {filepath}")
+    filedir, filename = os.path.split(filepath)
+    if filepath != "":
+        os.makedirs(filedir, exist_ok= True)
+        if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+            with open(filepath, 'w') as f:
+                pass
+        else:
+            print(f"file is already present at: {filepath}")
