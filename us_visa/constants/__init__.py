cadc3b5c0fa8cdef3b954828183952679c71bf2d
Fawad Khan <Fawadkhan1234>
2024-09-22 12:03:36 +0500

    model training, model evaluation, model pusher to s3 bucket, prediction pipeline

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -24,7 +24,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "us-east-1"
+REGION_NAME = "global"
 
 
 """
@@ -63,3 +63,14 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2023"
+MODEL_PUSHER_S3_KEY = "model-registry"
+BUCKET_REGION_NAME = "us-eat-1"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
